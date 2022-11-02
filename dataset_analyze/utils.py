da0459608195029259d1813690bf597e6e88ed86
Ziyue Zhang <ziyue.zhang@tum.de>
2022-11-02 11:35:26 +0100

    add g-values to analyze

--- dataset_analyze/utils.py
+++ dataset_analyze/utils.py
@@ -1,8 +1,8 @@
 import os
 import json
-from sklearn.feature_extraction import img_to_graph
+# from sklearn.feature_extraction import img_to_graph
 from PIL import Image, ImageDraw, ImageColor, ImageFont
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
 
@@ -24,3 +24,29 @@ def split_annos(TypeKeys:list, Path:str):
             annos.append(json.load(f))    
     # annotation = list(zip(fnames, annos))
     return fnames, annos
+
+
+def split_g(label:str):
+    '''
+    Note: this is only for signs infor, warn, comple, regu
+    to split signs with same g value
+    we have g1-12, g15, g25, g45 in total 15
+    
+    '''
+    num = 99
+    if label[-2] == 'g':
+        num = int(label[-1])-1
+    elif (label[-2:]<='12'):
+        num = int(label[-2:])-1
+    elif (label[-2:]>'12'):
+        if label[-2:]=='15':
+            num = 12
+        elif label[-2:] == '25':
+            num = 13
+        elif label[-2:] == '45':
+            num = 14
+        else:
+            print('error', label)
+    else:
+        print('error', label)
+    return num
